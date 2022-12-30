4cafc8122eff79632904d60cbc61bc9abcb11016
Yuichi Tanaka <yuichielectric@gmail.com>
2022-12-30 12:35:45 +0900

    Update basic_cleaning to drop inproper geolocation

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -34,6 +34,10 @@ def go(args):
     logger.info("Convert last_review to datetime")
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    logger.info("Drop rows in the dataset that are not in the proper geolocation")
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     with tempfile.TemporaryDirectory() as tmp_dir:
         temp_path = os.path.join(tmp_dir, args.output_artifact)
 
