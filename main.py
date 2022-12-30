b41b655f5e3dfe19302ec310210ed65491e562aa
Yuichi Tanaka <yuichielectric@gmail.com>
2022-12-30 12:26:38 +0900

    Add test_regression_model step

--- main.py
+++ main.py
@@ -114,12 +114,15 @@ def go(config: DictConfig):
             )
 
         if "test_regression_model" in active_steps:
-
-            ##################
-            # Implement here #
-            ##################
-
-            pass
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/test_regression_model",
+                "main",
+                version="main",
+                parameters={
+                    "mlflow_model": "random_forest_export:prod",
+                    "test_dataset": "test_data.csv:latest",
+                }
+            )
 
 
 if __name__ == "__main__":
